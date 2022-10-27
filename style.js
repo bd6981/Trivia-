f15dbda221c6034b6f8a6633eec92d8697bc794c
bd6981 <brittanyjdavis22@gmail.com>
2022-10-27 13:40:34 -0400

    fixed buttons getting stuck

--- style.js
+++ style.js
@@ -1,7 +1,7 @@
-/*----------JavaScript Code for Trivia Game-------*/
 
-//-\/---------------Elements--------------\/
-const nextBtn = document.getElementById("next");
+
+//-----Button,Elements,EventListeners----\/
+// const nextBtn = document.getElementById("next");
 const restartBtn = document.getElementById("restart");
 const submitBtn = document.getElementById("submit");
 const userScore = document.getElementById("user-score");
@@ -9,31 +9,29 @@ const totalScore = document.getElementById("total-score");
 const questionText= document.getElementById("question-text");
 const trueBtn = document.getElementById("True");
 const falseBtn = document.getElementById("False");
-//-----------------Elements------------->----^
 
-//---\/--------------Events-----------------\/    
 restartBtn.addEventListener("click",restart);
+// nextBtn.addEventListener("click",next);
 submitBtn.addEventListener("click",submit);
-//-------------------Events------------->----^
+//--------------------------------------------------
 
-//---\/--------Trivia Questions-------------\/
-
+//---\/----trivia questions--------\/
 let currentQuestion = 1;
 let score = 0;
+
 let questions = [
     {
         question:"What rock star moonlights as a horror movie writer and director?",
         answers:[
-            {option:"Rob Zombie",answer: true},
-            {option:"Ozzy Osbourne",answer: false},
+            {option:"Rob Zombie",answer:true},
+            {option:"Ozzy Osbourne",answer:false},
         ]
-    
     },
     {
         question:"Who was the first woman ever inducted into the Rock and Roll Hall of Fame??",
         answers:[
-            {option:"Dolly Parton",answer: false},
-            {option:"Aretha Franklin",answer: true},
+            {option:"Dolly Parton",answer:false},
+            {option:"Aretha Franklin",answer:true},
         ]
     },
     {
@@ -120,7 +118,7 @@ let questions = [
             {option:"Sting",answer:false},
         ]
     }]
-//-----------------------Trivia Questions------------------->----^
+//--------------------------------------------------------------
 
 //--------\/-------------Let The Games Begin--------------------\/
 function beginGame() {
@@ -134,9 +132,6 @@ function beginGame() {
                 score++;
             }
         }
-
-   
-        
         userScore.innerHTML = score;
         if(currentQuestion < 1) {
             next();
@@ -150,8 +145,6 @@ function beginGame() {
                 score++;
             }
         }
- 
-        
         userScore.innerHTML = score;
         if(currentQuestion < 1) {
             next();
@@ -161,25 +154,27 @@ function beginGame() {
  }
  
  beginGame();
-//-----------------------Begin--------------------->----^
+//--------------------------------------------------------
 
-
-//-------\/------------RestartGame--------------------\/
+//-------\/----------------RestartGame-----------------\/
 function restart() {
     currentQuestion = 0;
+    // nextBtn.classList.remove("hide");
     submitBtn.classList.remove("hide");
     trueBtn.classList.remove("hide");
     falseBtn.classList.remove("hide");
     score = 0;
-    userScore.innerHTML = score.value;
+    userScore.innerHTML = score;
     beginGame();
  } 
-//----------------------Restart------------------->----^
+//------------------------------------------------------------
 
 //--------\/--------Next Question Please--------------\/
 function next() {
     currentQuestion++;
     if(currentQuestion >= 1) {
+       ;
+    }
     questionText.innerHTML = questions[currentQuestion].question;
     trueBtn.innerHTML = questions[currentQuestion].answers[0].option;
     trueBtn.onclick = () => {
@@ -188,7 +183,6 @@ function next() {
                 score++;
             }
         }
-    }}       
         userScore.innerHTML = score;
         if(currentQuestion < 14) {
             next();
@@ -209,19 +203,20 @@ function next() {
         }
     }
   
+}
+//------------------------------------------------------------------
 
-//-------------------------Next----------------------->----^
-
-//-------------\/------Submit answers-------------------\/
+//-------------\/------Submit answers------\/
 function submit() {
+    // nextBtn.classList.add("hide");
     submitBtn.classList.add("hide");
     trueBtn.classList.add("hide");
     falseBtn.classList.add("hide");   
     questionText.innerHTML = "Just remember, You're the rockstar!"
  }
-//------------------------submit--------------------->----^
+//------------------------------------------------------------------
 
-//-------\/-------working on random shuffle below------------\/
+//-------\/-------working on random shuffle below-----------------\/
 //     shuffledQuestions = question.Sort(()  => Math.random() - .5)
 //     currentQuestionIndex = 0
 //     qContainer.classList.remove('hide')
@@ -238,4 +233,4 @@ function submit() {
 //         }
 //         answerContainer.addEventListener('click', answerButtons)
 //         answerOptions.appendChild(button)
-//        
+//  
